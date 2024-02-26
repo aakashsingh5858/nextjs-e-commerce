e5f85f49a4dec76108152c82fe97a1f4192375e4
akashsingh <aakashsingh2702@gmail.com>
2024-02-26 19:56:58 +0530

    first commit

--- app/components/ProductList.js
+++ app/components/ProductList.js
@@ -76,8 +76,8 @@ export default function ProductList({
       </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 my-14 px-5 md:px-0">
         {isLoading ? (
-          Array.apply(null, Array(10)).map((a, index) => <ProductSkeleton key={index} />)
-        ) : sortedProduct.length !== 0 ? (
+          Array.apply(null, Array(10))?.map((a, index) => <ProductSkeleton key={index} />)
+        ) : sortedProduct?.length !== 0 ? (
           sortedProduct?.map((data, i) => {
             return (
               <ProductCard product={data} key={i} isWishItem={isWishItem} />
